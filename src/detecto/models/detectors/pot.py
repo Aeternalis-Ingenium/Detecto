f6e214b6af39fd865c7f7f2f5daf3bc99fe5415e
N. L. <nino@pleno.earth>
2023-10-30 21:57:16 +0100

    feat(detectors): implement compute_pot_threshold and extract_exceedances

--- src/detecto/models/detectors/pot.py
+++ src/detecto/models/detectors/pot.py
@@ -1,19 +1,38 @@
 from pandas import DataFrame
 
 from src.detecto.models.detectors.interface import Detecto
+from src.detecto.models.timeframes.pot import POTTimeframe
 
 
-class OTPDetecto(Detecto):
+class POTDetecto(Detecto):
+    def __init__(self):
+        self.timeframe = POTTimeframe()
+        self.pot_threshold_quantile = 0.99
+        self.pot_threshold = None
+        self.anomaly_threshold_quantile = 0.99
+        self.anomaly_threshold = None
+        self.params = {}
+
+    def compute_pot_threshold(self, dataset: DataFrame) -> None:
+        if not self.timeframe.t0:
+            self.timeframe.set_interval(total_rows=dataset.shape[0])
+        self.pot_threshold = (
+            dataset.expanding(min_periods=self.timeframe.t0).quantile(q=self.pot_threshold_quantile).bfill()
+        )
+
+    def extract_exceedances(self, dataset: DataFrame) -> DataFrame:
+        return dataset.subtract(self.pot_threshold).clip(lower=0)
+
     def fit(self, dataset: DataFrame | list) -> None:  # type: ignore
         pass
 
     def score(self, dataset: DataFrame | list) -> DataFrame:  # type: ignore
         pass
 
-    def predict(self, dataset: DataFrame | list) -> DataFrame:  # type: ignore
+    def detect(self, dataset: DataFrame | list) -> DataFrame:  # type: ignore
         pass
 
-    def evaluate(self, dataset: DataFrame | list, prediction: DataFrame | list) -> DataFrame:  # type: ignore
+    def evaluate(self, dataset: DataFrame | list, detected: DataFrame | list) -> DataFrame:  # type: ignore
         pass
 
     def set_params(self, **params: dict) -> None:  # type: ignore
@@ -21,3 +40,6 @@ class OTPDetecto(Detecto):
 
     def get_params(self) -> dict:  # type: ignore
         pass
+
+    def __str__(self):
+        return "Peak Over Threshold Anomaly Detector"
