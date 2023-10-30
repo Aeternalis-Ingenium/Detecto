a2039114be9ec4ee2e703076b1e797067536124e
N. L. <nino@pleno.earth>
2023-10-30 17:39:27 +0100

    feat(tests): create AbstractTimeframeTestModel for testing the Timeframe abstract class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from pandas import DataFrame
 
 from src.detecto.models.detectors.interface import Detecto
+from src.detecto.models.timeframes.interface import Timeframe
 
 
 class AbstractDetectoTestModel(Detecto):
@@ -10,10 +11,10 @@ class AbstractDetectoTestModel(Detecto):
     def score(self, dataset: DataFrame | list) -> DataFrame:
         pass
 
-    def predict(self, dataset: DataFrame | list) -> DataFrame:
+    def detect(self, dataset: DataFrame | list) -> DataFrame:
         pass
 
-    def evaluate(self, dataset: DataFrame | list, prediction: DataFrame | list) -> DataFrame:
+    def evaluate(self, dataset: DataFrame | list, detected: DataFrame | list) -> DataFrame:
         pass
 
     def set_params(self, **params: dict) -> None:
@@ -21,3 +22,8 @@ class AbstractDetectoTestModel(Detecto):
 
     def get_params(self) -> dict:
         return {"param_1": "Test Param 1", "param_2": "Test Param 2"}
+
+
+class AbstractTimeframeTestModel(Timeframe):
+    def set_interval(self, **params: dict[str, int | float | bool]) -> None:
+        pass
