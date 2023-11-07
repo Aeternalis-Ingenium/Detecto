f52f86eb4e8f3bf29f074ffd15b6056f89a2249b
N. L. <nino@pleno.earth>
2023-11-07 23:21:19 +0100

    fix(models): Adjust all params in all methods in `POTDetecto`

--- src/detecto/models/detectors/pot.py
+++ src/detecto/models/detectors/pot.py
@@ -5,39 +5,68 @@ from src.detecto.models.timeframes.pot import POTTimeframe
 
 
 class POTDetecto(Detecto):
+    """
+    POTDetecto class implements the Peaks Over Threshold (POT) approach for anomaly detection.
+
+    # Attributes
+        * timeframe (POTTimeframe): Instance managing the timeframe details for the POT method.
+        * __params (dict): Private dictionary to store parameters after model fitting.
+    """
+
     def __init__(self):
         self.timeframe = POTTimeframe()
-        self.pot_threshold_quantile = 0.99
-        self.pot_threshold = None
-        self.anomaly_threshold_quantile = 0.99
-        self.anomaly_threshold = None
-        self.params = {}
+        self.__params = {}
 
-    def compute_pot_threshold(self, dataset: DataFrame) -> None:
-        if not self.timeframe.t0:
-            self.timeframe.set_interval(total_rows=dataset.shape[0])
-        self.pot_threshold = (
-            dataset.expanding(min_periods=self.timeframe.t0).quantile(q=self.pot_threshold_quantile).bfill()
-        )
+    def compute_exceedance_threshold(self, dataset: DataFrame, q: float = 0.99) -> DataFrame:
+        """
+        Calculate the exceedance threshold for each feature in the dataset.
 
-    def extract_exceedances(self, dataset: DataFrame) -> DataFrame:
-        return dataset.subtract(self.pot_threshold).clip(lower=0)
+        # Parameters
+            * dataset (DataFrame): The dataset to calculate the threshold for.
+            * q (float): The quantile to use for thresholding.
 
-    def fit(self, dataset: DataFrame | list) -> None:  # type: ignore
+        # Returns
+            * DataFrame: The threshold values for each feature.
+        """
+        return dataset.expanding(min_periods=self.timeframe.t0).quantile(q=q).bfill()
+
+    def extract_exceedance(
+        self,
+        dataset: DataFrame,
+        exceedance_threshold_dataset: DataFrame,
+        fill_value: float | None = 0.0,
+        clip_lower: float | None = 0.0,
+    ) -> DataFrame:
+        """
+        Extract values from the dataset that exceed the threshold values.
+
+        # Parameters
+            * dataset (DataFrame): The original dataset to compare against thresholds.
+            * exceedance_threshold_dataset (DataFrame): Calculated thresholds for the dataset.
+            * fill_value (float | None): Value to fill missing entries with before comparison.
+            * clip_lower (float | None): Minimum value to clip data to after subtraction.
+
+        # Returns
+            * DataFrame: The dataset with values exceeding the thresholds.
+        """
+        return dataset.subtract(exceedance_threshold_dataset, fill_value=fill_value).clip(lower=clip_lower)
+
+    def fit(self, dataset: DataFrame, **kwargs: DataFrame | list | int | float | None) -> DataFrame:
         pass
 
-    def score(self, dataset: DataFrame | list) -> DataFrame:  # type: ignore
+    def compute_anomaly_threshold(self, dataset: DataFrame):
         pass
 
-    def detect(self, dataset: DataFrame | list) -> DataFrame:  # type: ignore
+    def detect(self, dataset: DataFrame, **kwargs: DataFrame | list | int | float | None) -> DataFrame:
         pass
 
-    def evaluate(self, dataset: DataFrame | list, detected: DataFrame | list) -> DataFrame:  # type: ignore
+    def evaluate(self, dataset: DataFrame, **kwargs: DataFrame | list | int | float | None) -> DataFrame:
         pass
 
-    def set_params(self, **params: dict) -> None:  # type: ignore
+    def set_params(self, **kwargs: int | float | str | None) -> None:
         pass
 
+    @property
     def get_params(self) -> dict:  # type: ignore
         pass
 
