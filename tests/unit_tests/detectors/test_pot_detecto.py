7f8722dba9148dbd3a4886cd7911a6b608bc2081
N. L. <nino@pleno.earth>
2023-11-07 23:23:55 +0100

    fix(tests): rewrite unit tests for `pot` into `exceedance`
    Adjusting the naming convention of the keyword `pot` into `exceedance`

--- tests/unit_tests/detectors/test_pot_detecto.py
+++ tests/unit_tests/detectors/test_pot_detecto.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
-from pandas import DataFrame, Series, testing
+from pandas import DataFrame, testing as pd_testing
+from scipy.stats import genpareto
 
 from src.detecto.models.detectors.interface import Detecto
 from src.detecto.models.detectors.pot import POTDetecto
@@ -10,67 +11,67 @@ class TestPOTDetecto(TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.detector = POTDetecto()
-        self.df = DataFrame(
+        self.df_1 = DataFrame(
             data={
-                "feature_1": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-                "feature_2": [15, 17, 24, 36, 23, 15, 75, 56, 89, 105],
+                "df_1_feature_1": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+                "df_1_feature_2": [15, 17, 24, 36, 23, 15, 75, 56, 89, 105],
             }
+        )
+        self.df_2 = DataFrame(
+            {
+                "df_2_feature_1": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+                "df_2_feature_2": [15, 25, 35, 45, 55, 65, 75, 85, 95, 105],
+            }
+        )
+        self.detector.timeframe.set_interval(
+            total_rows=self.df_1.shape[0],
         )
 
     def test_construct_pot_detecto_successful(self):
-        assert isinstance(self.detector, Detecto)
-        assert str(self.detector) == "Peak Over Threshold Anomaly Detector"
-        self.assertIsNone(self.detector.fit(dataset=[1, 2, 3]))  # type: ignore
-        self.assertIsNone(self.detector.score(dataset=[1, 2, 3]))
-        self.assertIsNone(self.detector.detect(dataset=[1, 2, 3]))
-        self.assertIsNone(self.detector.evaluate(dataset=[3000, 8, 24, 6000], detected=[1, 0, 0, 1]))
-        self.assertIsNone(self.detector.set_params())  # type: ignore
-
-    def test_pot_threshold_quantile_default_value(self):
-        assert type(self.detector.pot_threshold_quantile) == float  # type: ignore
-        assert self.detector.pot_threshold_quantile == 0.99  # type: ignore
-
-    def test_anomaly_threshold_quantile_default_value(self):
-        assert type(self.detector.anomaly_threshold_quantile) == float  # type: ignore
-        assert self.detector.anomaly_threshold_quantile == 0.99  # type: ignore
-
-    def test_compute_pot_threshold(self):
-        self.detector.compute_pot_threshold(dataset=self.df)
-
-        expected_pot_threshold = DataFrame(
+        expected_detected_df = DataFrame(
             data={
-                "feature_1": [59.5, 59.5, 59.5, 59.5, 59.5, 59.5, 69.4, 79.3, 89.2, 99.1],
-                "feature_2": [35.40, 35.40, 35.40, 35.40, 35.40, 35.40, 72.66, 73.67, 87.88, 103.56],
+                "is_anomaly_col_1": [False, False, False, False, False, False, False, False, True, True],
+                "is_anomaly_col_2": [False, False, False, False, True, False, False, False, False, False],
             }
         )
 
-        testing.assert_series_equal(
-            left=self.detector.pot_threshold["feature_1"], right=expected_pot_threshold["feature_1"]  # type: ignore
+        assert isinstance(self.detector, Detecto)
+        assert str(self.detector) == "Peak Over Threshold Anomaly Detector"
+        self.assertIsNone(self.detector.evaluate(dataset=self.df_1, detected=expected_detected_df))
+
+    def test_compute_exceedance_threshold(self):
+        exceedance_threshold_df = self.detector.compute_exceedance_threshold(dataset=self.df_1, q=0.99)
+
+        expected_exceedance_threshold = DataFrame(
+            data={
+                "df_1_feature_1": [59.5, 59.5, 59.5, 59.5, 59.5, 59.5, 69.4, 79.3, 89.2, 99.1],
+                "df_1_feature_2": [35.40, 35.40, 35.40, 35.40, 35.40, 35.40, 72.66, 73.67, 87.88, 103.56],
+            }
         )
-        testing.assert_series_equal(
-            left=self.detector.pot_threshold["feature_2"], right=expected_pot_threshold["feature_2"]  # type: ignore
+
+        pd_testing.assert_frame_equal(left=exceedance_threshold_df, right=expected_exceedance_threshold)
+        pd_testing.assert_series_equal(
+            left=exceedance_threshold_df["df_1_feature_1"], right=expected_exceedance_threshold["df_1_feature_1"]  # type: ignore
+        )
+        pd_testing.assert_series_equal(
+            left=exceedance_threshold_df["df_1_feature_2"], right=expected_exceedance_threshold["df_1_feature_2"]  # type: ignore
         )
 
     def test_extract_exeedances(self):
-        expected_pot_threshold = DataFrame(
+        expected_threshold_df = DataFrame(
             data={
-                "feature_1": [59.5, 59.5, 59.5, 59.5, 59.5, 59.5, 69.4, 79.3, 89.2, 99.1],
-                "feature_2": [35.40, 35.40, 35.40, 35.40, 35.40, 35.40, 72.66, 73.67, 87.88, 103.56],
+                "df_1_feature_1": [59.5, 59.5, 59.5, 59.5, 59.5, 59.5, 69.4, 79.3, 89.2, 99.1],
+                "df_1_feature_2": [35.40, 35.40, 35.40, 35.40, 35.40, 35.40, 72.66, 73.67, 87.88, 103.56],
             }
         )
 
-        self.detector.compute_pot_threshold(dataset=self.df)
+        exceedance_threshold_df = self.detector.compute_exceedance_threshold(dataset=self.df_1, q=0.99)
 
-        testing.assert_series_equal(
-            left=self.detector.pot_threshold["feature_1"], right=expected_pot_threshold["feature_1"]  # type: ignore
-        )
-        testing.assert_series_equal(
-            left=self.detector.pot_threshold["feature_2"], right=expected_pot_threshold["feature_2"]  # type: ignore
-        )
+        pd_testing.assert_frame_equal(left=exceedance_threshold_df, right=expected_threshold_df)
 
-        expected_exceedances = DataFrame(
+        expected_exceedance_df = DataFrame(
             data={
-                "feature_1": [
+                "df_1_feature_1": [
                     0,
                     0,
                     0,
@@ -82,7 +83,7 @@ class TestPOTDetecto(TestCase):
                     0.7999999999999972,
                     0.9000000000000057,
                 ],
-                "feature_2": [
+                "df_1_feature_2": [
                     0,
                     0,
                     0,
@@ -96,15 +97,19 @@ class TestPOTDetecto(TestCase):
                 ],
             }
         )
-        exceedances = self.detector.extract_exceedances(dataset=self.df)
+        exceedance_df = self.detector.extract_exceedance(
+            dataset=self.df_1, exceedance_threshold_dataset=exceedance_threshold_df, fill_value=0.0, clip_lower=0.0
+        )
 
-        testing.assert_series_equal(left=exceedances["feature_1"], right=expected_exceedances["feature_1"])
-        testing.assert_series_equal(left=exceedances["feature_2"], right=expected_exceedances["feature_2"])
+        pd_testing.assert_frame_equal(left=exceedance_df, right=expected_exceedance_df)
+        pd_testing.assert_series_equal(
+            left=exceedance_df["df_1_feature_1"], right=expected_exceedance_df["df_1_feature_1"]
+        )
+        pd_testing.assert_series_equal(
+            left=exceedance_df["df_1_feature_2"], right=expected_exceedance_df["df_1_feature_2"]
+        )
 
     def test_genpareto_fitting_method(self):
-        pass
-
-    def test_score_method(self):
         pass
 
     def test_detect_method(self):
@@ -120,4 +125,4 @@ class TestPOTDetecto(TestCase):
         pass
 
     def tearDown(self) -> None:
-        pass
+        return super().tearDown()
