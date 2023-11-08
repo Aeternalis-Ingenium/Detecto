9b93029f6c4700c869e64e7a37e0ff03a5be813d
N. L. <nino@pleno.earth>
2023-11-08 10:11:36 +0100

    fix(tests): rewrite the test for testing the abstract `Detecto` class

--- tests/unit_tests/detectors/test_detecto.py
+++ tests/unit_tests/detectors/test_detecto.py
@@ -17,27 +17,19 @@ class TestDetectoInterfaceClass(TestCase):
         )
 
     def test_fit(self):
-        self.assertIsNone(self.detector.fit(dataset=self.test_df))  # type: ignore
+        self.assertIsNone(self.detector.fit(dataset=self.test_df))
 
     def test_detect(self):
         self.assertIsNone(self.detector.detect(dataset=self.test_df))
 
     def test_evaluate(self):
-        expected_detected_df = DataFrame(
-            data={
-                "is_anomaly_col_1": [False, False, False, False, False, False, False, False, True, True],
-                "is_anomaly_col_2": [False, False, False, False, True, False, False, False, False, False],
-            }
-        )
-        self.assertIsNone(self.detector.evaluate(dataset=self.test_df, detected=expected_detected_df))
+        self.assertIsNone(self.detector.evaluate(dataset=self.test_df))
 
     def test_set_params(self):
-        self.assertTrue(self.detector.set_params())  # type: ignore
+        self.assertIsNone(self.detector.set_params())  # type: ignore
 
     def test_get_params(self):
-        expected_params = {"param_1": "Test Param 1", "param_2": "Test Param 2"}
-
-        assert self.detector.get_params == expected_params
+        self.assertIsNone(self.detector.params)
 
     def tearDown(self) -> None:
         return super().tearDown()
