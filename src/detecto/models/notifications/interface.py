2ac57a71052a78f060f3609eaa06c9fae6075067
N. L. <nino@pleno.earth>
2023-11-23 23:09:57 +0100

    feat(models): add parameter types for Notification method

--- src/detecto/models/notifications/interface.py
+++ src/detecto/models/notifications/interface.py
@@ -1,9 +1,10 @@
 from abc import ABCMeta, abstractmethod
+from datetime import datetime
 
 
 class Notification(metaclass=ABCMeta):
     @abstractmethod
-    def setup(self, data) -> None:
+    def setup(self, data: list[dict[str, str | float | int | datetime]], message: str) -> None:
         """_summary_
 
         Args:
