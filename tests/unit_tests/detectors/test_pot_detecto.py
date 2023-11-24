f4579db22492746b7923259e57177cc809712754
N. L. <nino@pleno.earth>
2023-11-24 01:36:03 +0100

    fix(test): change assertTrue() to assertEqual()

--- tests/unit_tests/detectors/test_pot_detecto.py
+++ tests/unit_tests/detectors/test_pot_detecto.py
@@ -24,7 +24,7 @@ class TestPOTDetecto(TestCase):
         self.assertIsInstance(obj=self.detector, cls=Detecto)
 
     def test_string_method(self):
-        self.assertTrue(expr=str(self.detector) == "Peak Over Threshold Anomaly Detector")
+        self.assertEqual(first=str(self.detector), second="Peak Over Threshold Anomaly Detector")
 
     def test_construct_pot_detecto_successful(self):
         expected_detected_df = DataFrame(
@@ -555,7 +555,7 @@ class TestPOTDetecto(TestCase):
 
         self.detector.compute_anomaly_threshold(dataset=anomaly_score_df, q=0.90)
 
-        self.assertTrue(expr=self.detector.anomaly_threshold == expected_anomaly_threshold)
+        self.assertEqual(first=self.detector.anomaly_threshold, second=expected_anomaly_threshold)
 
     def test_detect_method(self):
         expected_anomaly_df = DataFrame(
@@ -984,7 +984,7 @@ class TestPOTDetecto(TestCase):
 
         self.detector.compute_anomaly_threshold(dataset=anomaly_score_df, q=0.90)
 
-        self.assertTrue(expr=self.detector.anomaly_threshold == expected_anomaly_threshold)
+        self.assertEqual(first=self.detector.anomaly_threshold, second=expected_anomaly_threshold)
 
         anomaly_df = self.detector.detect(dataset=anomaly_score_df)
 
@@ -1023,8 +1023,8 @@ class TestPOTDetecto(TestCase):
         }
         self.detector.params[0] = []  # type: ignore
 
-        self.assertTrue(expr=type(self.detector.params) == dict)
-        self.assertTrue(expr=len(self.detector.params[0]) == 0)  # type: ignore
+        self.assertEqual(first=type(self.detector.params), second=dict)
+        self.assertEqual(first=len(self.detector.params[0]), second=0)  # type: ignore
 
         self.detector.set_params(
             feature_name="anomaly_score_col_1",
@@ -1048,9 +1048,9 @@ class TestPOTDetecto(TestCase):
 
         self.detector.set_params(feature_name="total_anomaly_score", row=0, total_anomaly_score_per_row=220.0)
 
-        self.assertTrue(expr=type(self.detector.params) == type(expected_params))
-        self.assertTrue(expr=len(self.detector.params[0]) == len(expected_params[0]))  # type: ignore
-        self.assertTrue(expr=self.detector.params[0] == expected_params[0])  # type: ignore
+        self.assertEqual(first=type(self.detector.params), second=type(expected_params))
+        self.assertEqual(first=len(self.detector.params[0]), second=len(expected_params[0]))  # type: ignore
+        self.assertEqual(first=self.detector.params[0], second=expected_params[0])  # type: ignore
 
     def tearDown(self) -> None:
         return super().tearDown()
--- tests/unit_tests/timeframes/test_pot_timeframe.py
+++ tests/unit_tests/timeframes/test_pot_timeframe.py
@@ -13,7 +13,7 @@ class TestPOTDetecto(TestCase):
         self.assertIsInstance(obj=self.timeframe, cls=Timeframe)
 
     def test_string_method(self):
-        self.assertTrue(expr=str(self.timeframe) == "Peak Over Threshold Timeframe")
+        self.assertEqual(first=str(self.timeframe), second="Peak Over Threshold Timeframe")
 
     def test_construct_pot_detecto_successful(self):
         self.assertIsNone(obj=self.timeframe.t0)
@@ -23,13 +23,13 @@ class TestPOTDetecto(TestCase):
     def test_pot_detecto_set_time_windows_method_dev_mode(self):
         self.timeframe.set_interval(total_rows=10000)
 
-        self.assertTrue(expr=self.timeframe.t0 == 6000)
-        self.assertTrue(expr=self.timeframe.t1 == 2500)
-        self.assertTrue(expr=self.timeframe.t2 == 1500)
+        self.assertEqual(first=self.timeframe.t0, second=6000)
+        self.assertEqual(first=self.timeframe.t1, second=2500)
+        self.assertEqual(first=self.timeframe.t2, second=1500)
 
     def test_pot_detecto_set_time_windows_method_prod_mode(self):
         self.timeframe.set_interval(total_rows=10000, prod_mode=True)
 
-        self.assertTrue(expr=self.timeframe.t0 == 5999)
-        self.assertTrue(expr=self.timeframe.t1 == 4000)
-        self.assertTrue(expr=self.timeframe.t2 == 1)
+        self.assertEqual(first=self.timeframe.t0, second=5999)
+        self.assertEqual(first=self.timeframe.t1, second=4000)
+        self.assertEqual(first=self.timeframe.t2, second=1)
