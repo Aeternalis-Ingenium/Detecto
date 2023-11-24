206ed234542d7944c0a098c4f98fd0f52152714b
N. L. <nino@pleno.earth>
2023-11-24 22:31:01 +0100

    feat(standalone): create standalone function to calculate the exceedances (pot data)

--- src/detecto/standalone/pot.py
+++ src/detecto/standalone/pot.py
@@ -14,3 +14,24 @@ def compute_pot_threshold(dataset: DataFrame, t0: int, q: float = 0.99) -> DataF
         * DataFrame: The threshold for each feature.
     """
     return dataset.expanding(min_periods=t0).quantile(q=q).bfill()
+
+
+def extract_pot_data(
+    dataset: DataFrame,
+    pot_threshold_dataset: DataFrame,
+    fill_value: float | None = 0.0,
+    clip_lower: float | None = 0.0,
+) -> DataFrame:
+    """
+    Extract values from the dataset that exceed the threshold values.
+
+    # Parameters
+        * dataset (DataFrame): The original dataset to compare against thresholds.
+        * pot_threshold_dataset (DataFrame): The DataFrame with POT thresholds to compute the exceedances.
+        * fill_value (float | None): Value to fill missing entries with before comparison.
+        * clip_lower (float | None): Minimum value to clip data to after subtraction.
+
+    # Returns
+        * DataFrame: The dataset with values exceeding the thresholds.
+    """
+    return dataset.subtract(pot_threshold_dataset, fill_value=fill_value).clip(lower=clip_lower)
