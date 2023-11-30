fe1905415e2369ac1aa43dbf5634eac5ddfebed3
N. L. <nino@pleno.earth>
2023-12-01 00:18:45 +0100

    feat(utils): create custom type for all `Detecto` classes

---
+++ src/detecto/utils/_types.py
@@ -0,0 +1,19 @@
+from src.detecto.models.detectors.autoencoder import AutoencoderDetecto
+from src.detecto.models.detectors.block_maxima import BlockMaximaDetecto
+from src.detecto.models.detectors.dbscan import DBSCANDetecto
+from src.detecto.models.detectors.isoforest import IsoForestDetecto
+from src.detecto.models.detectors.mad import MADDetecto
+from src.detecto.models.detectors.one_class_svm import OneClassSVMDetecto
+from src.detecto.models.detectors.pot import POTDetecto
+from src.detecto.models.detectors.zscore import ZScoreDetecto
+
+Detecto = (
+    AutoencoderDetecto
+    | BlockMaximaDetecto
+    | DBSCANDetecto
+    | IsoForestDetecto
+    | MADDetecto
+    | OneClassSVMDetecto
+    | POTDetecto
+    | ZScoreDetecto
+)
