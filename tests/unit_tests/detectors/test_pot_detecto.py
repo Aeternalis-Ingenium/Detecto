767e6b7e9c04ada7280de07c5d859f9b11fc8721
N. L. <nino@pleno.earth>
2023-11-30 19:55:10 +0100

    test(unit): implement kolmogorov smirnov for testing the genpareto.fit() resulting params
    The h0 in Kolmogorov Smirnov test is that 2 distributions are identical "F(x) == G(x)". This can be observed through the parameter "statistic" which is the measurement of the distance between 2 distributions (the bigger the value, the more different the distributions) and the p-value (the higher, the better).

--- tests/unit_tests/detectors/test_pot_detecto.py
+++ tests/unit_tests/detectors/test_pot_detecto.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from pandas import DataFrame, testing as pd_testing
+from scipy.stats import genpareto, ks_1samp
 
 from src.detecto.models.detectors.interface import Detecto
 from src.detecto.models.detectors.pot import POTDetecto
@@ -157,6 +158,45 @@ class TestPOTDetecto(TestCase):
         self.detector.fit(dataset=self.df_1)
 
         pd_testing.assert_frame_equal(left=self.detector.anomaly_score_dataset, right=expected_anomaly_score_df)
+
+        exceedance_feature_1_df = expected_exceedance_df["df_1_feature_1"].copy()
+        exceedance_feature_2_df = expected_exceedance_df["df_1_feature_2"].copy()
+        filtered_exceedance_feature_1_df = exceedance_feature_1_df[exceedance_feature_1_df > 0]
+        filtered_exceedance_feature_2_df = exceedance_feature_2_df[exceedance_feature_2_df > 0]
+
+        self.assertEqual(first=filtered_exceedance_feature_1_df.shape[0], second=5)
+        self.assertEqual(first=filtered_exceedance_feature_2_df.shape[0], second=4)
+
+        feature_1_nonzero_params = self.detector._POTDetecto__get_nonzero_params(feature_name="df_1_feature_1")  # type: ignore
+        feature_2_nonzero_params = self.detector._POTDetecto__get_nonzero_params(feature_name="df_1_feature_2")  # type: ignore
+
+        self.assertIsNotNone(obj=feature_1_nonzero_params)
+        self.assertIsNotNone(obj=feature_2_nonzero_params)
+
+        kstest_feature_1_result = ks_1samp(
+            x=filtered_exceedance_feature_1_df,
+            cdf=genpareto.cdf,
+            args=(
+                feature_1_nonzero_params[-1][1][0],
+                feature_1_nonzero_params[-1][1][1],
+                feature_1_nonzero_params[-1][1][2],
+            ),
+        )
+
+        kstest_feature_2_result = ks_1samp(
+            x=filtered_exceedance_feature_2_df,
+            cdf=genpareto.cdf,
+            args=(
+                feature_2_nonzero_params[-1][1][0],
+                feature_2_nonzero_params[-1][1][1],
+                feature_2_nonzero_params[-1][1][2],
+            ),
+        )
+
+        self.assertEqual(first=kstest_feature_1_result.statistic, second=0.3999983048907735)
+        self.assertEqual(first=kstest_feature_1_result.pvalue, second=0.30880444800752727)
+        self.assertEqual(first=kstest_feature_2_result.statistic, second=0.25)
+        self.assertEqual(first=kstest_feature_2_result.pvalue, second=0.90625)
 
     def test_fit_method_catches_value_error(self):
         self.detector.compute_exceedance_threshold(dataset=self.df_1, q=0.90)
@@ -1051,8 +1091,441 @@ class TestPOTDetecto(TestCase):
         with self.assertRaises(expected_exception=ValueError):
             self.detector.detect()
 
-    def test_evaluate_method(self):
-        pass
+    def test_evaluate_method_with_kolmogorov_smirnof(self):
+        test_df = DataFrame(
+            data={
+                "df_1_feature_1": [
+                    263,
+                    275,
+                    56,
+                    308,
+                    488,
+                    211,
+                    70,
+                    42,
+                    67,
+                    472,
+                    304,
+                    297,
+                    480,
+                    227,
+                    453,
+                    342,
+                    115,
+                    115,
+                    67,
+                    295,
+                    9,
+                    228,
+                    89,
+                    225,
+                    360,
+                    367,
+                    418,
+                    124,
+                    229,
+                    12,
+                    111,
+                    341,
+                    209,
+                    374,
+                    254,
+                    322,
+                    99,
+                    166,
+                    435,
+                    481,
+                    106,
+                    438,
+                    180,
+                    33,
+                    30,
+                    330,
+                    139,
+                    17,
+                    268,
+                    204000,
+                ],
+                "df_1_feature_2": [
+                    387,
+                    525,
+                    520,
+                    79,
+                    345000,
+                    474,
+                    336,
+                    159,
+                    164,
+                    110,
+                    308,
+                    434,
+                    197,
+                    470,
+                    411,
+                    100,
+                    33,
+                    205,
+                    229,
+                    295,
+                    599,
+                    535,
+                    364,
+                    24,
+                    476,
+                    582,
+                    7,
+                    418,
+                    33,
+                    149,
+                    303,
+                    11,
+                    359,
+                    484,
+                    339,
+                    69,
+                    267,
+                    297,
+                    19,
+                    69,
+                    56,
+                    533,
+                    225,
+                    524,
+                    596,
+                    304,
+                    181,
+                    535,
+                    369,
+                    351,
+                ],
+            }
+        )
+        self.detector.timeframe.set_interval(total_rows=test_df.shape[0])
+        expected_exceedance_threshold_df = DataFrame(
+            data={
+                "df_1_feature_1": [
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    454.90000000000003,
+                    453.0,
+                    449.50000000000006,
+                    446.0,
+                    442.5,
+                    439.00000000000006,
+                    435.5,
+                    431.99999999999994,
+                    428.50000000000017,
+                    438.6,
+                    454.90000000000003,
+                    453.0,
+                    451.5,
+                    450.00000000000006,
+                    448.50000000000006,
+                    447.0,
+                    445.5,
+                    444.0,
+                    442.50000000000006,
+                    441.00000000000006,
+                    454.90000000000003,
+                ],
+                "df_1_feature_2": [
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    539.7,
+                    535.0,
+                    534.0,
+                    533.0,
+                    532.0,
+                    531.0,
+                    530.0,
+                    529.0,
+                    528.0,
+                    527.0,
+                    526.0,
+                    525.0,
+                    532.2,
+                    531.4000000000001,
+                    530.6,
+                    534.2,
+                    534.0,
+                    533.8,
+                    535.0,
+                    535.0,
+                    535.0,
+                ],
+            }
+        )
+        expected_exceedance_df = DataFrame(
+            data={
+                "df_1_feature_1": [
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    33.099999999999966,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    17.099999999999966,
+                    0.0,
+                    0.0,
+                    25.099999999999966,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    26.099999999999966,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    203545.1,
+                ],
+                "df_1_feature_2": [
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    344460.3,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    59.299999999999955,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    42.299999999999955,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.7999999999999545,
+                    0.0,
+                    0.0,
+                    61.799999999999955,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                ],
+            }
+        )
+        expected_anomaly_score_df = DataFrame(
+            data={
+                "anomaly_score_df_1_feature_1": [
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    1.5947881128257808,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    float("inf"),
+                ],
+                "anomaly_score_df_1_feature_2": [
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    1.0092577188337257,
+                    0.0,
+                    0.0,
+                    2.1563458584349675,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                ],
+                "total_anomaly_score": [
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    1.5947881128257808,
+                    0.0,
+                    1.0092577188337257,
+                    0.0,
+                    0.0,
+                    2.1563458584349675,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    float("inf"),
+                ],
+            }
+        )
+        self.detector.compute_exceedance_threshold(dataset=test_df, q=0.90)
+
+        pd_testing.assert_frame_equal(
+            left=self.detector.exceedance_threshold_dataset, right=expected_exceedance_threshold_df
+        )
+
+        self.detector.extract_exceedance(
+            dataset=test_df,
+            fill_value=0.0,
+            clip_lower=0.0,
+        )
+
+        pd_testing.assert_frame_equal(left=self.detector.exceedance_dataset, right=expected_exceedance_df)
+
+        self.detector.fit(dataset=test_df)
+
+        pd_testing.assert_frame_equal(left=self.detector.anomaly_score_dataset, right=expected_anomaly_score_df)
+
+        self.detector.evaluate(method="ks", stat_distance_threshold=0.5)
+
+        expected_kstest_result = DataFrame(
+            data={
+                "feature": ["df_1_feature_1", "df_1_feature_2"],
+                "total_exceedances": [5, 5],
+                "stat_distance": [0.4, 0.3015702053031097],
+                "p_value": [0.3087999999999995, 0.6578151672671728],
+                "c": [-3.507231273303336, 5.13336319598767],
+                "loc": [0, 0],
+                "scale": [116.08935514634031, 6.263007768565435],
+                "is_identical": [True, True],
+            }
+        )
+
+        pd_testing.assert_frame_equal(left=self.detector.kstest_result, right=expected_kstest_result)
+
+        self.detector.evaluate(method="ks", stat_distance_threshold=0.3)
+        expected_kstest_result["is_identical"] = [False, False]
+
+        pd_testing.assert_frame_equal(left=self.detector.kstest_result, right=expected_kstest_result)
 
     def test_params_attributes(self):
         expected_params = {
